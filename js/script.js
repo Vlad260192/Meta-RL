a91673a218ac70ba92cba4b96646cb4a68c125e8
Vlad260192 <v.kapustianskiy@gmail.com>
2022-06-13 18:23:25 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Burger
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger,.header__menu-wrapper').toggleClass('active');
@@ -5,6 +6,9 @@ $(document).ready(function() {
     });
 });
 
+// -----------------
+
+// Tabs
 let tabs = document.querySelectorAll('.tabs .tab-btn');
 let tabContents = document.querySelectorAll('.tab-content .tab-option');
 
@@ -20,3 +24,16 @@ tabs.forEach((tab,index) => {
         tabs[index].classList.add('_tab-active');
     });
 });
+// ----------------
+
+// Spoiler
+$(document).ready(function() {
+    $('.spoiler-block__title').click(function(event) {
+        if($('.spoiler-block').hasClass('one')) {
+            $('.spoiler-block__title').not($(this)).removeClass('active');
+            $('.spoiler-block__text').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
+// ---------------
